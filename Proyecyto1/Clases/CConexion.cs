ab6a1f474c9c61e4f9caf2048c856f191813f1f6
DESKTOP-IFH2L3N\csi20-iiglhez <csi20-iiglhez@colegioaltair.net>
2023-03-22 12:28:51 +0100

    no message

---
+++ Proyecyto1/Clases/CConexion.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecyto1.Clases
+{
+     class CConexion
+    {
+        NpgsqlConnection conexion = new NpgsqlConnection();
+
+        static string servidor = "localhost";
+        static string bd = "pilotoPharma";
+        static string usuario = "postgres";
+        static string passw = "1234";
+        static string puerto = "5432";
+
+        String cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario +
+            ";" + "password=" + passw + ";" + "database=" + bd + ";";  
+
+
+        public NpgsqlConnection establecerConexion()
+        {
+            try
+            {
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+                Console.WriteLine("Se ha abierto la conexion");
+
+            }catch(NpgsqlException  e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        return conexion;
+        }
+    }
+}
---
+++ Proyecyto1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Proyecyto1.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecyto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CConexion c = new CConexion();
+            c.establecerConexion();
+            Console.ReadLine();
+        }
+    }
+}
